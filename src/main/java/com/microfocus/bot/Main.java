26d6a488db08f44b2caec7e189b4084388bb0cb5
skalinich <skalinich@serena.com>
2020-06-02 14:00:12 +0300

    Added parrot bot as sample

--- src/main/java/com/microfocus/bot/Main.java
+++ src/main/java/com/microfocus/bot/Main.java
@@ -1,4 +1,21 @@
 package com.microfocus.bot;
 
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+
 public class Main {
+
+    public static final String BOT_USER_NAME = "octane_hackathon2020_bot";
+    public static final String BOT_TOKEN = "1094483244:AAFlGShQHGuSLcV8SejjSXesuW27lC1k1CQ";
+
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new ParrotBot());
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
+    }
 }
---
+++ src/main/java/com/microfocus/bot/ParrotBot.java
@@ -0,0 +1,32 @@
+package com.microfocus.bot;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class ParrotBot extends TelegramLongPollingBot {
+
+    public void onUpdateReceived(Update update) {
+        System.out.println(update);
+
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage()
+                    .setChatId(update.getMessage().getChatId())
+                    .setText(update.getMessage().getText());
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public String getBotUsername() {
+        return Main.BOT_USER_NAME;
+    }
+
+    public String getBotToken() {
+        return Main.BOT_TOKEN;
+    }
+}
